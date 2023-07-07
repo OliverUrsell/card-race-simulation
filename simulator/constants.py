8b8c8821f0d22c3a1af75519985d40854028c8d8
OliverUrsell <ollieursell@hotmail.co.uk>
2023-07-07 12:08:16 +0100

    Change the default k_length_of_line to 7 instead of 3

--- simulator/constants.py
+++ simulator/constants.py
@@ -2,7 +2,7 @@
 from typing import Dict, Optional
 from simulator.card import Suit
 
-k_length_of_line = 3  # The length of the track the cards need to cover
+k_length_of_line = 7  # The length of the track the cards need to cover
 
 # k_extra_cards modifies the deck of the game to include more or less of a given suit
 # E.g: k_extra_cards: Dict[Suit, int] = {Suit.HEART: 2, Suit.DIAMOND: -1, Suit.SPADE: -2, Suit.CLUB: -10}
